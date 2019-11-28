161e0becc3955e77536b8aed9f29e528f52ed006
Ivan Fuertes <ivan.fuertes@wikitude.com>
2019-11-28 13:12:38 +0100

    Fixing the compilation of GLFW in windows for 3.3

--- cmake/deps_files.cmake
+++ cmake/deps_files.cmake
@@ -23,11 +23,11 @@ set(DEPS_SRC_FILES_WIN
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/win32_joystick.h
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/win32_monitor.c
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/win32_platform.h
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/win32_thread.c
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/win32_time.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/win32_tls.c
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/win32_window.c
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/wgl_context.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/wgl_context.h)
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/osmesa_context.c)
 
 #Linux Dependency Files
 set(DEPS_SRC_FILES_LINUX
@@ -38,9 +38,8 @@ set(DEPS_SRC_FILES_LINUX
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/x11_window.c
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/egl_context.c
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/posix_time.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/posix_tls.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/posix_thread.c
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/linux_joystick.c
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/glfw3.h
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/xkb_unicode.c)
 
 #MacOS Dependency Files
@@ -50,7 +49,7 @@ set(DEPS_SRC_FILES_MACOS
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/cocoa_monitor.m
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/cocoa_window.m
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/nsgl_context.m
-    ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/posix_tls.c
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/posix_thread.c
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/cocoa_joystick.h
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/cocoa_platform.h
     ${CMAKE_CURRENT_SOURCE_DIR}/src/deps/glfw/cocoa_time.c)
