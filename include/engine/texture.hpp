9c45fa48df160429a7e68bc1eeda8a179f901bf9
Ivan Fuertes <ivan.fuertes@wikitude.com>
2019-12-13 11:01:32 +0100

    Texture class refactored

--- include/engine/texture.hpp
+++ include/engine/texture.hpp
@@ -7,48 +7,51 @@
 class Shader;
 
 class Texture {
-public:
-    enum class Wrap {
-        Repeat,
-        Mirrored_Repeat,
-        Clamp_To_Border,
-        Clamp_To_Edge
-    };
+    public:
+        enum class Wrap {
+            Repeat,
+            Mirrored_Repeat,
+            Clamp_To_Border,
+            Clamp_To_Edge
+        };
 
-    enum class Filter {
-        None,
-        Nearest,
-        Linear
-    };
+        enum class Filter {
+            None,
+            Nearest,
+            Linear
+        };
 
-    enum class Format {
-        RGB,
-        RGBA
-    };
+        enum class Format {
+            RGB,
+            RGBA
+        };
 
-    Texture(const char* path, Format format, const std::string& name = "");
-    Texture() = delete;
-    ~Texture();
+        Texture(const char* path, Format format);
+        Texture() = delete;
+        ~Texture();
 
-    Texture(const Texture&) = default;
-    Texture(Texture&&) = default;
-    Texture& operator=(const Texture&) = default;
-    Texture& operator=(Texture&&) = default;
+        Texture(const Texture&) = default;
+        Texture(Texture&&) = default;
+        Texture& operator=(const Texture&) = default;
+        Texture& operator=(Texture&&) = default;
 
-    void activate(const Shader& shader, uint32_t unit = 0, const char* name = nullptr);
+        void activate(const Shader& shader, const char* name, uint32_t unit = 0) const;
 
-    void setWrap(Wrap s, Wrap t) {}
-    void setFilter(Filter mag, Filter min, Filter mipMapMag = Filter::None, Filter mipMapMin = Filter::None) {}
+        void setWrap(Wrap s, Wrap t);
+        void setFilter(Filter mag, Filter min, Filter mipMapMag = Filter::None, Filter mipMapMin = Filter::None);
 
-private:
-    std::string _name = "";
-    uint32_t _id = 0;
-    Format _format = Format::RGB;
-    int32_t _width = 0, _height = 0;
-    int32_t _depth = 0;
-    std::pair<Wrap, Wrap>_wrap{ Wrap::Repeat, Wrap::Repeat };
-    std::pair<Filter, Filter>_filter{ Filter::Linear, Filter::Linear };
-    std::pair<Filter, Filter>_filterMipMap{ Filter::None, Filter::None };
+    private:
+        void applyWrapping() const;
+        void applyFilter() const;
+
+    private:
+        uint32_t _id = 0;
+        Format _format = Format::RGB;
+        int32_t _width = 0, _height = 0;
+        int32_t _depth = 0;
+        std::pair<Wrap, Wrap>_wrap{ Wrap::Repeat, Wrap::Repeat };
+        std::pair<Filter, Filter>_filter{ Filter::Linear, Filter::Linear };
+        std::pair<Filter, Filter>_filterMipMap{ Filter::None, Filter::None };
 };
 
 #endif
--- src/engine/texture.cpp
+++ src/engine/texture.cpp
@@ -1,41 +1,95 @@
 #include "engine/texture.hpp"
-#include <glad/glad.h>
-#include "stb_image.h"
-#include <iostream>
 #include "engine/shader.hpp"
 
-Texture::Texture(const char* path, Format format, const std::string& name) : _name(name) , _format(format){
+#include <glad/glad.h>
+#include <iostream>
+#include <stb_image.h>
+
+static uint32_t format2GL(Texture::Format format) {
+    switch (format) {
+    case Texture::Format::RGB: return GL_RGB;
+    case Texture::Format::RGBA: return GL_RGBA;
+    default:return GL_RGB;
+    };
+}
+
+Texture::Texture(const char* path, Format format) : _format(format) {
+    stbi_set_flip_vertically_on_load(true);
+
     glGenTextures(1, &_id);
     glBindTexture(GL_TEXTURE_2D, _id);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    stbi_set_flip_vertically_on_load(true);
+    applyWrapping();
+    applyFilter();
 
     unsigned char* data = stbi_load(path, &_width, &_height, &_depth, 0);
     if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, _width, _height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, format2GL(_format), _width, _height, 0, format2GL(_format), GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
         stbi_image_free(data);
     } else {
-        std::cout << "Error loading texture " << path << std::endl;
+        std::cout << "Failed to Load Texture " << path << std::endl;
     }
+
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 Texture::~Texture() {
     glDeleteTextures(1, &_id);
 }
 
-void Texture::activate(const Shader& shader, uint32_t unit, const char* name) {
+void Texture::setWrap(Wrap s, Wrap t) {
+    _wrap = std::make_pair(s, t);
+    applyWrapping();
+}
+
+void Texture::setFilter(Filter mag, Filter min, Filter mipMapMag, Filter mipMapMin) {
+    _filter = std::make_pair(mag, min);
+    _filterMipMap = std::make_pair(mipMapMag, mipMapMin);
+    applyFilter();
+}
+
+static uint32_t wrap2GL(Texture::Wrap wrap) {
+    switch (wrap) {
+        case Texture::Wrap::Repeat: return GL_REPEAT;
+        case Texture::Wrap::Mirrored_Repeat: return GL_MIRRORED_REPEAT;
+        case Texture::Wrap::Clamp_To_Border: return GL_CLAMP_TO_BORDER;
+        case Texture::Wrap::Clamp_To_Edge: return GL_CLAMP_TO_EDGE;
+        default: return GL_REPEAT;
+    };
+}
+
+void Texture::applyWrapping() const {
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap2GL(_wrap.first));
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap2GL(_wrap.second));
+}
+
+static uint32_t filter2GL(Texture::Filter filter, Texture::Filter mipMap) {
+    switch (filter) {
+        case Texture::Filter::Nearest: switch (mipMap) {
+            case Texture::Filter::None: return GL_NEAREST;
+            case Texture::Filter::Nearest: return GL_NEAREST_MIPMAP_NEAREST;
+            case Texture::Filter::Linear: return GL_NEAREST_MIPMAP_LINEAR;
+            default: return GL_NEAREST;
+        };
+        case Texture::Filter::Linear: switch (mipMap) {
+            case Texture::Filter::None: return GL_LINEAR;
+            case Texture::Filter::Nearest: return GL_LINEAR_MIPMAP_NEAREST;
+            case Texture::Filter::Linear: return GL_LINEAR_MIPMAP_LINEAR;
+            default: return GL_LINEAR;
+        };
+        case Texture::Filter::None: return GL_NEAREST;
+        default: return GL_NEAREST;
+    }
+}
+
+void Texture::applyFilter() const {
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filter2GL(_filter.first, _filterMipMap.first));
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filter2GL(_filter.second, _filterMipMap.second));
+}
+
+void Texture::activate(const Shader& shader, const char* name, uint32_t unit) const {
     glActiveTexture(GL_TEXTURE0 + unit);
     glBindTexture(GL_TEXTURE_2D, _id);
-    if (name) {
-        shader.set(name, static_cast<int32_t>(unit));
-    } else {
-        shader.set(_name.c_str(), static_cast<int32_t>(unit));
-    }
+    shader.set(name, static_cast<int32_t>(unit));
 }
